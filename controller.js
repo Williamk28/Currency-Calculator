084908ada63189f6e7d22d0114d881275532e0b7
William Kong <william.kong.2018@uni.strath.ac.uk>
2021-02-02 15:59:58 +0000

    Stage 4 Completed

--- controller.js
+++ controller.js
@@ -7,22 +7,23 @@ const initialise = evt => {
     model = new Model();
     view = new View();
     view.getStorage();
+    model.getLiveRates();
     //do any initialisation and "plumbing" here
 
     view.buttonClick((event)=>{
         if (event.target.id === "clear"){
             model.clear();
         } else if (event.target.id === "equals"){
-            let away = view.getCurrency(view.getAwayCurrency());
-            let home = view.getCurrency(view.getHomeCurrency());
+            let away = model.getAwayRates(view.getAwayCurrency());
+            let home = model.getHomeRates(view.getHomeCurrency());
             model.convert(model.visitDisplay, away, home, view.bankFee());
         }
         else {
             model.appendNumber(event.target.id);
         }
         view.visitOutput(model.visitDisplay,model.homeDisplay);
-        view.setStorage();
     });
+    view.setStorage();
 };
 
 
--- model.js
+++ model.js
@@ -1,5 +1,8 @@
 'use strict';
 
+let currency = [];
+let rates = [];
+
 class Model {
     constructor(){
         this.visitDisplay = "";
@@ -25,4 +28,50 @@ class Model {
         this.homeDisplay = result;
     }
 
+    getLiveRates(){
+        let i;
+        let xmlText = "";
+        let ECBRates;
+        let parser;
+        let xmlDoc;
+        let request = new XMLHttpRequest();
+        request.addEventListener("load", function() {
+            xmlText = request.responseText;
+            parser = new DOMParser();
+            xmlDoc = parser.parseFromString(xmlText, "text/xml");
+            ECBRates = xmlDoc.getElementsByTagName("Cube")[0].getElementsByTagName("Cube")[0];
+            for (i = 0; i < 32; i++){
+                currency[i] = ECBRates.getElementsByTagName("Cube")[i].getAttribute("currency");
+                rates[i] = ECBRates.getElementsByTagName("Cube")[i].getAttribute("rate");
+            }
+            currency.push("EUR");
+            rates.push("1");
+        });
+        console.log(currency);
+        console.log(rates);
+        request.open("GET", "https://devweb2020.cis.strath.ac.uk/~aes02112/ecbxml.php", true);
+        request.send();
+    }
+
+    getAwayRates(away){
+        let i;
+        let awayRate;
+        for (i = 0; i < currency.length; i++) {
+            if (currency[i] === away){
+                awayRate = rates[i];
+            }
+        }
+        return awayRate;
+    }
+
+    getHomeRates(home){
+        let i;
+        let homeRate;
+        for (i = 0; i < currency.length; i++){
+            if (currency[i] === home){
+                homeRate = rates[i];
+            }
+        }
+        return homeRate;
+    }
 }
